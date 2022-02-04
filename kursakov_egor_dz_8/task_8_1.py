6e29096403aa06c4e184a44c16df90636fce72b1
Avery46 <kursakovegor46@gmail.com>
2022-02-05 01:30:25 +0300

    les8

---
+++ kursakov_egor_dz_8/task_8_1.py
@@ -0,0 +1,13 @@
+import re
+VALID_RE = re.compile(
+    r'(?P<username>([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+)@(?P<domain>[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+)')
+
+
+def email_val(email):
+    try:
+        test = list(map(lambda x: x.groupdict(), VALID_RE.finditer(email)))
+        print(test[0])
+    except:
+        raise ValueError from ValueError
+
+print(email_val(input('')))
